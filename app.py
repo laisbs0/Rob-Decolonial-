a3978a5f5cac14460db135179c9ffd81f6237212
Lais <114266022+laisbs0@users.noreply.github.com>
2023-04-14 12:03:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open("credenciais.json", mode="w") as arquivo:
 conta = ServiceAccountCredentials.from_json_keyfile_name("credenciais.json")
 api = gspread.authorize(conta)
 planilha = api.open_by_key("11OkESzP3BYZqkuKGJM4sW4YvywJKNeA6lEr9ReQ3b0U")
-sheet = planilha.worksheet("países")
+sheet = planilha.worksheet("País")
 log = planilha.worksheet("chat")
 
 app = Flask(__name__)
@@ -42,32 +42,29 @@ def contato():
   return menu + "laisbatistasantana@gmail.com"
 
 #Rota para receber as atualizações do bot do Telegram
+#Criei um Bot do Telegram que avisa se o país enviado pelo usuário já foi ou não invadido pela Inglaterra. O código a seguir é a rota criada para ser processada pelo Render e automatizar o robô. Contudo, o bot não funciona como deveria e segue enviando a mesma mensagem o tempo todo, independente da mensagem do usuário. Devolva o código corrigido.
 @app.route("/telegram-bot", methods=["POST"])
 def telegram_bot():
-  update = request.json
-  chat_id = update["message"]["chat"]["id"]
-  message = update["message"]["text"]
-  paises_nao_invadidos = sheet.get_all_records()
+    update = request.json
+    chat_id = update["message"]["chat"]["id"]
+    message = update["message"]["text"]
+    paises_nao_invadidos = [registro["País"] for registro in sheet.get_all_records()]
 
-  if message == "/start":
-    replies = ["Bem-vindo(a)! Aqui te ajudo a descobrir quais países foram e não foram invadidos pela Inglaterra. Qual você quer saber?"]
-  elif message in paises_nao_invadidos:
-    replies = ["O país " + message + " nunca foi invadido pela Inglaterra."]
-  else:
-    replies = ["Isso aí já foi invadido pela Inglaterra. Haha. (☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)"]
+    if message == "/start":
+        replies = ["Bem-vindo(a)! Aqui te ajudo a descobrir quais países foram e não foram invadidos pela Inglaterra. Qual você quer saber?"]
+    elif message in paises_nao_invadidos:
+        replies = ["O país " + message + " nunca foi invadido pela Inglaterra."]
+    else:
+        replies = ["Isso aí já foi invadido pela Inglaterra. Haha. (☞ﾟヮﾟ)☞ ☜(ﾟヮﾟ☜)"]
 
-  if message != "/start":
-    replies.append("Pergunte sobre outro país")
-      
-  for reply in replies: 
-    nova_mensagem = {"chat_id": chat_id, "text": reply}
-    requests.post(f"https://api.telegram.org./bot{TELEGRAM_TOKEN_BOT}/sendMessage", data=nova_mensagem)
-    #log.append_row([datahora, "enviada", username, first_name, chat_id, texto_resposta,])
-    #time.sleep(10)
-    #message = update["message"]["text"]
+    if message != "/start":
+        replies.append("Pergunte sobre outro país")
 
+    for reply in replies:
+        nova_mensagem = {"chat_id": chat_id, "text": reply}
+        requests.post(f"https://api.telegram.org./bot{TELEGRAM_TOKEN_BOT}/sendMessage", data=nova_mensagem)
 
-#Página do mapinha
+#Página do mapinha que vou criar
 @app.route("/mapa")
 def mapa():
   return menu + "oi"
